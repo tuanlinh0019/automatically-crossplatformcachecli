64d604dc405b8658cfcecbb52539d51402132114
tuanlinh0019 <jordiano11pun0suy@hotmail.com>
2024-05-10 15:23:30 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-function isPrime(n) {
-  if (n <= 1) return false;
-  if (n <= 3) return true;
-  if (n % 2 === 0 || n % 3 === 0) return false;
-  let i = 5;
-  while (i * i <= n) {
-    if (n % i === 0 || n % (i + 2) === 0) return false;
-    i += 6;
+function groupAnagrams(strs) {
+  const map = new Map();
+  for (const str of strs) {
+    const sortedStr = str.split("").sort().join("");
+    if (!map.has(sortedStr)) map.set(sortedStr, []);
+    map.get(sortedStr).push(str);
   }
-  return true;
+  return [...map.values()];
 }
