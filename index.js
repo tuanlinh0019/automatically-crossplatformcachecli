587343b46c36c41a1c443a05cd9f48844fdd2550
tuanlinh0019 <jordiano11pun0suy@hotmail.com>
2024-05-13 02:06:00 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-function isSubsequence(s, t) {
-  let i = 0;
-  let j = 0;
-  while (i < s.length && j < t.length) {
-    if (s[i] === t[j]) i++;
-    j++;
-  }
-  return i === s.length;
-}
+const difference = (a, b) => {
+  const s = new Set(b);
+  return a.filter((x) => !s.has(x));
+};
