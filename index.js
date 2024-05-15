1ba1554098d5e1f9cb24bbc6f25e2af34a5b9733
tuanlinh0019 <jordiano11pun0suy@hotmail.com>
2024-05-16 06:42:39 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-function isPalindrome(str) {
-  const reversed = str.split("").reverse().join("");
-  return str === reversed;
-}
-
-console.log(isPalindrome("racecar")); // true
+console.log("hello");
